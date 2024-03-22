0b9d0e856c6e917607ba4ed0f97a36324f034b04
d.brown1 <brown.d@wehi.edu.au>
2024-03-22 14:37:01 +1100

    hive scripts

---
+++ scripts/bash/data_in_brief/HIVE_AGI_15_beenet.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --partition=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=110G
+#SBATCH --time=6:00:00
+#SBATCH -e bee-slurm-%j.err
+#SBATCH -o bee-slurm-%j.out
+#SBATCH --job-name=AGI_15
+
+# USAGE
+# ./beenet analyze --sample-name=... --ref=... --num-barcodes=# <fq.gz files>
+
+SAMPLE="AGI_15";
+BARCODES="10000";
+REF="/stornext/HPCScratch/home/brown.d/Refs/HIVE/20210603_GRCh38.104";
+OUTDIR="/stornext/Projects/score/2021_Sequencing_Runs/NN247/beenet/AGI_15";
+
+/stornext/HPCScratch/home/brown.d/beenet analyze \
+    --sample-name=$SAMPLE \
+    --ref=$REF \
+    --num-barcodes=$BARCODES \
+    --out=$OUTDIR \
+    /stornext/Projects/score/2021_Sequencing_Runs/NN247/fastq/HIVE_AGI_15_S3_L001_R1_001.fastq.gz \
+    /stornext/Projects/score/2021_Sequencing_Runs/NN247/fastq/HIVE_AGI_15_S3_L001_R2_001.fastq.gz;
